cbdd9927df7bb33b13fdc2948e4fa687ed70b1d2
DimitriDaSilva <dimitri.gomes.da.silva@gmail.com>
2023-01-29 16:07:19 +0100

    chore: remove css modules

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,4 +1,3 @@
-// import './globals.css'
 import "./output.css";
 
 export default function RootLayout({
@@ -8,10 +7,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
       <head />
       <body>{children}</body>
     </html>
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import styles from "./page.module.css";
 
 export default function Home() {
   return (
@@ -12,19 +11,6 @@ export default function Home() {
           This is a Tailwind Example with Next.js 13
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src='/vercel.svg' alt='Vercel Logo' width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   );
 }
