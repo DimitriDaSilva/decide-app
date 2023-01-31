773f557ae2b2716a0d7bdf5d0a069647244ba059
DimitriDaSilva <dimitri.gomes.da.silva@gmail.com>
2023-01-31 21:49:37 +0100

    feat: add prisma module

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class AuthService {
+  constructor(private prismaService: PrismaService) {}
+
   login() {
     return { message: 'login' };
   }
