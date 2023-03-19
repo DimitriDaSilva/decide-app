162ee5c0174b4bcc158d7481650c10dc76655ffc
DimitriDaSilva <dimitri.gomes.da.silva@gmail.com>
2023-03-19 09:40:44 +0100

    chore: import LoggingInterceptor in app module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { AuthModule } from './auth/auth.module';
+import { LoggingInterceptor } from './interceptors/logging.interceptor';
 import { PrismaModule } from './prisma/prisma.module';
 import { TableModule } from './table/table.module';
 
@@ -12,6 +14,12 @@ import { TableModule } from './table/table.module';
     AuthModule,
     PrismaModule,
     TableModule,
+  ],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
+    },
   ],
 })
 export class AppModule {}
