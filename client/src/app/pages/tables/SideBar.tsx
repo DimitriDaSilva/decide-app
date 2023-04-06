fead2a1a7c35e8d8d2e4fbeab53c45c12be0ddf5
DimitriDaSilva <dimitri.gomes.da.silva@gmail.com>
2023-04-06 08:50:31 +0200

    Add content to sidebar

--- client/src/app/pages/tables/SideBar.tsx
+++ client/src/app/pages/tables/SideBar.tsx
@@ -1,11 +1,27 @@
+import { Link } from 'react-router-dom';
+
 import { ReactComponent as TextLogo } from '@/assets/logos/logo-text.svg';
 import { ResizeableWidth } from '@/app/components/sidebar/ResizeableWidth';
+import { Button } from '@/app/components/button/Button';
 
 const SideBar = () => {
   return (
     <ResizeableWidth>
-      <div className="flex flex-1 w-full justify-between">
-        <TextLogo className="w-28" />
+      <div className="flex flex-1 w-full flex-col justify-between p-4">
+        <div className="flex flex-col gap-y-10 w-full items-center">
+          <TextLogo className="w-28 self-start" />
+
+          <Button variant="filled" color="gradient">
+            Add a new table
+          </Button>
+
+          <div>
+            <p>Tables</p>
+            <ul>
+              <Link to="/tables/1">Table title</Link>
+            </ul>
+          </div>
+        </div>
       </div>
     </ResizeableWidth>
   );
