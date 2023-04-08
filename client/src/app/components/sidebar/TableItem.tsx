1d0da9d3fa39e11aef693738b45577579385a55d
DimitriDaSilva <dimitri.gomes.da.silva@gmail.com>
2023-04-08 19:07:15 +0200

    Add tooltip to sidebar items

--- client/src/app/components/sidebar/TableItem.tsx
+++ client/src/app/components/sidebar/TableItem.tsx
@@ -1,5 +1,12 @@
 import { Link } from 'react-router-dom';
 
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '../Tooltip';
+
 type TableItemProps = {
   title: string;
   id: number;
@@ -8,7 +15,14 @@ type TableItemProps = {
 const TableItem = ({ title, id }: TableItemProps) => {
   return (
     <Link to={`/tables/${id}`}>
-      <p className="truncate w-full">{title}</p>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger>
+            <p className="truncate w-full">{title}</p>
+          </TooltipTrigger>
+          <TooltipContent side="right">{title}</TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
     </Link>
   );
 };
